e4c2430ff5353714d930398abe1b2cf881323061
Ryan Assis <ryan@assis.co.uk>
2016-07-23 17:21:18 +0100

    Initial Commit -
    Setup project Structure
    Basic Main Menu

---
+++ Assets/SuperDuperSpork/Scripts/UI/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpaceGame.UI
+{
+
+    public class MenuManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void joinGame()
+        {
+            Debug.Log("Join Game - I DO NOTHING YET");
+        }
+
+        public void exitGame()
+        {
+            Application.Quit();
+            Debug.Log("Exit");
+        }
+    }
+}
